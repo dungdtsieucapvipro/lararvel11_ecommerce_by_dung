7bd917080b0829d41291ca194d7f3f3dfc6336bf
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-11-13 12:53:46 +0700

    7th commit

--- resources/views/admin/brands.blade.php
+++ resources/views/admin/brands.blade.php
@@ -6,7 +6,7 @@
                 <h3>Brands</h3>
                 <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
                     <li>
-                        <a href="{{route('admin.index')}}">
+                        <a href="{{ route('admin.index') }}">
                             <div class="text-tiny">Dashboard</div>
                         </a>
                     </li>
@@ -31,16 +31,16 @@
                             </div>
                         </form>
                     </div>
-                    <a class="tf-button style-1 w208" href="{{route('admin.brand.add')}}">
+                    <a class="tf-button style-1 w208" href="{{ route('admin.brand.add') }}">
                         <i class="icon-plus"></i>Add new
                     </a>
                 </div>
                 
                 <div class="wg-table table-all-user">
                     <div class="table-responsive">
-                        @if (session::has('status'))
-                        <p class="alert alert-success">{{Session::get('status')}}</p>
-                        @enddif
+                        @if (Session::has('status'))
+                            <p class="alert alert-success">{{ Session::get('status') }}</p>
+                        @endif
                         <table class="table table-striped table-bordered">
                             <thead>
                                 <tr>
@@ -54,16 +54,16 @@
                             <tbody>
                                 @foreach ($brands as $brand)    
                                 <tr>
-                                    <td>{{$brand->id}}</td>
+                                    <td>{{ $brand->id }}</td>
                                     <td class="pname">
                                         <div class="image">
-                                            <img src="{{asset('uploads/brands')}}/{{$brand->image}}" alt="{{$brand->name}}" class="image">
+                                            <img src="{{ asset('uploads/brands/' . $brand->image) }}" alt="{{ $brand->name }}" class="image">
                                         </div>
                                         <div class="name">
-                                            <a href="#" class="body-title-2">{{$brand->name}}</a>
+                                            <a href="#" class="body-title-2">{{ $brand->name }}</a>
                                         </div>
                                     </td>
-                                    <td>{{$brand->slug}}</td>
+                                    <td>{{ $brand->slug }}</td>
                                     <td><a href="#" target="_blank">0</a></td>
                                     <td>
                                         <div class="list-icon-function">
@@ -86,11 +86,10 @@
                     </div>
                     <div class="divider"></div>
                     <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-                        {{$brands->links('pagination::bootstrap-5')}}
+                        {{ $brands->links('pagination::bootstrap-5') }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
-
