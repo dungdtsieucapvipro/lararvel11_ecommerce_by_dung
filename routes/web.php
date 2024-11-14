ee261b11482ee75b26cce4dc1f198f448a74db61
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-11-14 20:25:59 +0700

    14th commit

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,6 @@ Route::middleware(['auth', AuthAdmin::class])->group(function () {
     Route::get("/admin/category/edit/{id}", [AdminController::class, 'category_edit'])->name('admin.category.edit');
     Route::put("/admin/category/update", [AdminController::class, 'category_update'])->name('admin.category.update');
     Route::delete("/admin/category/delete/{id}", [AdminController::class, 'category_delete'])->name('admin.category.delete');
+
+    Route::get('/admin/products', [AdminController::class, 'products'])->name('admin.products');
 });
