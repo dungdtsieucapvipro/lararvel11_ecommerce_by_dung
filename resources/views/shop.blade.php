206d41f7753db35c30d7d5403d5afd39e6db9427
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-11-17 05:16:53 +0700

    23th commit loi ch bam dc nut show & default

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -334,17 +334,12 @@
 
             </select>
 
-            <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0" aria-label="Sort Items"
-              name="total-number">
-              <option selected>Default Sorting</option>
-              <option value="1">Featured</option>
-              <option value="2">Best selling</option>
-              <option value="3">Alphabetically, A-Z</option>
-              <option value="3">Alphabetically, Z-A</option>
-              <option value="3">Price, low to high</option>
-              <option value="3">Price, high to low</option>
-              <option value="3">Date, old to new</option>
-              <option value="3">Date, new to old</option>
+            <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0" aria-label="Sort Items" name="orderby" id="orderby" >
+              <option value="-1" {{ $order == -1 ? 'selected':''}}>Default</option>
+              <option value="1" {{ $order == 1 ? 'selected':''}}>Date, New To Old</option>
+              <option value="2" {{ $order == 2 ? 'selected':''}}>Date, Old To New selling</option>
+              <option value="3" {{ $order == 3 ? 'selected':''}}>Price, Low To High</option>
+              <option value="4" {{ $order == 4 ? 'selected':''}}>Price, High To Low</option>
             </select>
 
             <div class="shop-asc__seprator mx-3 bg-light d-none d-md-block order-md-0"></div>
@@ -464,6 +459,7 @@
   <form id="frmfilter" method="GET" action="{{route('shop.index')}}">
     <input type="hidden" name="page" value="{{$products->currentPage()}}">
     <input type="hidden" name="size" id="size" value="{{$size}}">
+    <input type="hidden" name="order" id="order" value="{{$order}}">
   </form>
 @endsection
 
@@ -473,7 +469,12 @@
       $("#pagesize").on("change",function(){
         $("#size").val($("#pagesize option:selected").val());
         $("#frmfilter").submit();
-      })
+      });
+
+      ${"orderby"}.on("change",function(){
+        $("#order").val($("#orderby option:selected").val());
+        $("#frmfilter").submit();
+      });
     });
   </script>
 @endpush
