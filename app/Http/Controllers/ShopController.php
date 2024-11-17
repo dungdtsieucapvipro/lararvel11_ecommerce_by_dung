a4b7da6258cd80c80e02128c4706f2349a581be7
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-11-17 10:05:26 +0700

    26th commit

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -17,6 +17,9 @@ class ShopController extends Controller
         $order = $request->query('order') ? $request->query('order') : -1;
         $f_brands = $request->query('brands');
         $f_categories = $request->query('categories');
+        $min_price = $request->query('min') ? $request->query('min') : 1;
+        $max_price = $request->query('max') ? $request->query('max') : 500;
+
         switch ($order) {
             case 1:
                 $o_column = 'created_at';
@@ -43,11 +46,15 @@ class ShopController extends Controller
         $products = Product::where(function ($query) use ($f_brands) {
             $query->whereIn('brand_id', explode(',', $f_brands))->orWhereRaw("'" . $f_brands . "'=''");
         })
+            ->where(function ($query) use ($min_price, $max_price) {
+                $query->whereBetween('regular_price', [$min_price, $max_price])
+                    ->orWhereBetween('sale_price', [$min_price, $max_price]);
+            })
             ->where(function ($query) use ($f_categories) {
                 $query->whereIn('category_id', explode(',', $f_categories))->orWhereRaw("'" . $f_categories . "'=''");
             })
             ->orderBy($o_column, $o_order)->paginate($size);
-        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories'));
+        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories', 'min_price', 'max_price'));
     }
 
     public function product_details($product_slug)
