bd3863c405be5b9edf600f88801220ff1fceb41a
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-11-18 21:14:52 +0700

    36th commit

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,8 @@ Route::delete('/cart/clear', [CartController::class, 'empty_cart'])->name('cart.
 
 
 Route::post('/cart/apply-coupon', [CartController::class, 'apply_coupon_code'])->name('cart.coupon.apply');
+Route::delete('/cart/remove-coupon', [CartController::class, 'remove_coupon_code'])->name('cart.coupon.remove');
+
 
 
 Route::post('/wishlist/add', [WishlistController::class, 'add_to_wishlist'])->name('wishlist.add');
